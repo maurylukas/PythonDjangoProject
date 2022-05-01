2c61b59be4e0a36cadb2317330699b718b5102f4
Maury Lukas Oliveira Freitas <97759783+maurylukas@users.noreply.github.com>
2022-04-30 21:50:24 -0300

    First user
    Creating super user

--- serie/models.py
+++ serie/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+LIST = (
+    ('Analises', 'Análises'),
+    ('Programacao', 'Programação'),
+    ('Apresentacao', 'Apresentação'),
+    ('Outros', 'Outros'),
+)
+
+
+class Serie (models.Model):
+    title = models.CharField (max_length = 100)
+    icon = models.ImageField (upload_to = 'icons')
+    description = models.TextField (max_length = 1000)
+    category = models.CharField (max_length = 15, choices = LIST)
+    views = models.IntegerField (default = 0)
+    date = models.DateTimeField (default = timezone.now)
+
+    def __str__(self):
+        return self.title

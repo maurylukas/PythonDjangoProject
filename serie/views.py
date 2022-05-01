dfc958a4b2ed74afa5cb95874f9671fc5d3368b4
Maury Lukas Oliveira Freitas <97759783+maurylukas@users.noreply.github.com>
2022-05-01 14:09:29 -0300

    First content
    Building page content

--- serie/views.py
+++ serie/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def homepage (request):
     return render(request, 'homepage.html')
+
+
+def homefilmes (request):
+    return render(request, 'homefilmes.html')
